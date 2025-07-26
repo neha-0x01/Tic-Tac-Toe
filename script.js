e48cfc232f13b163c4ce5025d3e228fef0f76508
Neha Kumari <neha.kumari80923@gmail.com>
2025-07-26 20:57:19 +0530

    add js

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Welcome to Tic Tac Toe");
+
+let music = new Audio("music.mp3");
+let turn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let btn = "X";
+
+// function to change the turn
+
+const changeTurn = () =>{
+    return turn === "X"?"0": "X"
+}
+
+// function to check  for a win
+const checkWin = ()=>{
+
+}
+
+
+//game logic
